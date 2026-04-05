8cb4c814b904f5df26c03e6da3e956118814857a
73RafaelSilva <73rafael.luiz@gmail.com>
2026-04-05 18:41:53 -0300

    Progresso no sistema de autenticação e cadastro, faltando apenas integração funcional com o banco, integração com verificação de email e Regex em todos os campos de input.

--- php/autenticacao.php
+++ php/autenticacao.php
@@ -1,27 +1,51 @@
 <?php
+//inicia sessao pho e informa o retorno ao js como json
+session_start();
+header("Content-Type: application/json");
 
-//include('conexao.php');
+//importa conexao com o banco de outro arquivo
+include('conexao.php');
 
-//$conexao = new mysqli("localhost:3306", "root", "", "cloud");
-
+if($_SERVER["REQUEST_METHOD"] === "POST"){
     $usuario_email = trim($_POST("email"));
     $usuario_senha = $_POST("senha");
-
-    $email = "usuario_email";
-    $senha = "usuario_senha";
-    $tabela = "usuario";
-
     
-    $stmt = conexao -> prepare("SELECT $senha FROM $tabela where $email = ?"); //monta pesquisa no banco
-    $stmt ->  bind_param("s", $usuario_email); // adciona entrada do usuario a busca negando sqli
+
+    $stmt = $conexao -> prepare("SELECT usuario_senha, usuario_cep, usuario_nome, usuario_telefone FROM $tabela where $email = ?"); //monta pesquisa no banco
+    $stmt ->  bind_param("s", $usuario_email); // adciona entrada do usuario a busca negando SQLI
     $stmt -> execute();
-    $senha = $stmt -> get_result();
+    $usuario = $stmt -> get_result();
 
-    if ($usuario_senha == $senha){
-        echo json_encode(["success" => true, "mensagem" => "Autenticado com sucesso"]);
-    } else {
-        echo json_encode(["success" => false, "mensagem" => "Falha na autenticação"]);
+    //checa se usuario encontrado e transforma resultado em lista digerivel pelo php
+    if($usuario = $usuario -> fetch_assoc()){
+        //se a senha encontrada for igual a senha digitada:
+        if(password_verify($usuario_senha, $usuario["usuario_senha"])){
+            //guarda dados em sessão
+            $_SESSION["usuario_nome"] = $usuario["usuario_nome"];
+            $_SESSION["usuario_email"] = $usuario["usuario_email"];
+            $_SESSION["usuario_cep"] = $usuario["usuario_cep"];
+            $_SESSION["usuario_telefne"] = $usuario["usuario_telefone"];
+
+            //responde sucesso ao js 
+            echo json_encode([
+                "success" => true, 
+                "mensagem" => "Autenticado com sucesso!"
+            ]);
+        } else {
+            // responde erro ao js
+            echo json_encode([
+                "success" => false,
+                "mensagem" => "Usuario ou senha incorretos!"
+            ]);
+        }
+    } else{
+        // responde erro ao js
+        echo json_encode([
+            "success" => false, 
+            "mensagem" => "Usuario ou senha incorretos!"
+        ]);
+
     }
-
-
-?>
+    exit;
+}
+?> 
--- php/cadastro_usuario.php
+++ php/cadastro_usuario.php
@@ -1,8 +1,11 @@
 <?php 
+//inicia sessao pho e informa o retorno ao js como json
+session_start();
+header('Content-Type: application/json');
 
-//include('conexao.php');
+//importa conexao com o banco de outro arquivo
+include('conexao.php');
 
-//$conexao = new mysqli("localhost:3306", "root", "", "cloud");
 
 if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $usuario_nome = $_POST["nome"];
@@ -11,6 +14,31 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
     $usuario_cep = $_POST["cep"];
     $usuario_senha1 = $_POST["senha1"];
     $usuario_senha2 = $_POST["senha2"];
+
+    //valida sintaxe do email
+    if (!preg_match("/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i", $email)) {
+            echo json_encode(["success" => false, "mensage" => "E-mail inválido!"]);
+            exit;
+    }
+
+    //guarda dados em sessão local
+    $_SESSION["usuario_nome"] = $usuario_nome;
+    $_SESSION["usuario_email"] = $usuario_email;
+    $_SESSION["usuario_cep"] = $usuario_cep;
+    $_SESSION["usuario_telefne"] = $usuario_telefone;
+}
+    //mensagem de sucesso ao js
+    echo json_encode([
+        "success" => true, 
+        "mensage" => "Cadastro realizado com sucesso! Bem-vindo, $usuario_nome."
+    ]);
+
+/*
+
+
+//$conexao = new mysqli("localhost:3306", "root", "", "cloud");
+
+
 
     $email_cadastrado = false;
 
@@ -34,4 +62,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST"){
          echo json.encode(["success" => true, "mensagem" =>"Proceguindo com o cadastrAs senhas não conhecidem.o..."]); 
     }// o programa apenas grava informações ao confirmar email.
 }
+*/
 ?>
