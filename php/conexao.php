8d380ab610fe101a375edf9a896fc0f4dfc860b0
andreeewee <151948490+andreeewee@users.noreply.github.com>
2026-04-06 19:24:52 -0300

    Adição do arquivo de conexão do banco

--- php/conexao.php
+++ php/conexao.php
@@ -1,3 +1,12 @@
 <?php
+    $usuario = 'root';
+    $senha = 'oitdbem';
+    $database = 'banco';
+    $host = 'localhost';
 
+    $mysqli = new mysqli ($host, $usuario, $senha, $database);
+
+    if($mysqli->error) {
+        die("Falha ao conectar ao banco de dados: " . $mysqli->error);
+    }
 ?>
