06f0a9718805aa07491f0e02f28fa2c0ac7b9e0a
andreeewee <151948490+andreeewee@users.noreply.github.com>
2026-04-08 15:08:26 -0300

    corrigir formulário da página de confirmaemail

--- telas/confirma_e-mail/confirma_e-mail.php
+++ telas/confirma_e-mail/confirma_e-mail.php
@@ -1,32 +1,44 @@
 <?php
-
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Confirmar E-Mail</title>
+    <title>Confirmar E-mail</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <div class="card">
-        <h2>Confirme seu e-mail</h2>
-        <p>Digite o e-mail utilizado no cadastro para receber o link de confirmação.</p>
+<div class="card">
 
-        <form>
-            <div class="input-group">
-                <label for="email">E-mail de confirmação:</label>
-                <input type="email" id="email" required placeholder="exemplo@gmail.com">
-            </div>
+    <h2>Confirme seu e-mail</h2>
+    <p>Digite o e-mail utilizado no cadastro para receber o link de confirmação.</p>
 
-            <button type="submit">Enviar Link de Confirmação</button>
-        </form>
+    <form action="../../mailer_test/mailer.php" method="POST">
 
-        <div class="footer-link">
-            Errou o e-mail? <a href="../cadastro_usuario.html">Voltar para o cadastro</a>
+        <div class="input-group">
+            <label for="email">E-mail de confirmação:</label>
+            <input 
+                type="email" 
+                name="email" 
+                id="email" 
+                required 
+                placeholder="exemplo@gmail.com"
+            >
         </div>
+
+        <button type="submit">Enviar Link de Confirmação</button>
+
+    </form>
+
+    <div class="footer-link">
+        Errou o e-mail? 
+        <a href="../cadastro_usuario.html">Voltar para o cadastro</a>
     </div>
-    <script src="script.js"></script>
+
+</div>
+
+<script src="confirma_e-mail.js"></script>
+
 </body>
 </html>
