be8dede1256a7e547504e91b79337ead50c61314
andreeewee <151948490+andreeewee@users.noreply.github.com>
2026-04-08 15:06:14 -0300

    corrigir configuração SMTP do PHPMailer

--- mailer_test/mailer.php
+++ mailer_test/mailer.php
@@ -1,32 +1,62 @@
 <?php
-    require "PHPMailer/src/PHPMailer.php";
-    require "PHPMailer/src/Exception.php";
-    require "PHPMailer/src/SMTP.php";
 
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+require "PHPMailer/src/PHPMailer.php";
+require "PHPMailer/src/Exception.php";
+require "PHPMailer/src/SMTP.php";
 
-    $mail = new PHPMailer();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-    //Configuração
-    $mail->Mailer = "smtp";
-    $mail->IsSMTP();
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPDebug = 0;
-    $mail->SMTPAuth = true;
-    $mail->SMTPSecure = 'ssl';
-    $mail->Host = 'smtp.gmail.com';
-    $mail->Port = 465;
+if(isset($_POST['email'])){
 
-    //Detalhes do envio de E-Mail
-    $mail->Username = "bazaronline.expcri@gmail.com";
-    $mail->Password = "nyrj lbvw rvgb oqzt";
-    $mail->setForm('bazaronline.expcri@gmail.com', "Doe Amor");
+    $email = $_POST['email'];
 
-    $mail->addAddress("bazaronline.expcri@gmail.com", "");
+    $mail = new PHPMailer(true);
 
-    $mail->Subject = "Light";
-    $mail->msgHTML("<h1></h1>");
+    try {
 
-    //Chamar biblioteca com $mail->send();
+        //Configuração SMTP
+        $mail->isSMTP();
+        $mail->CharSet = "UTF-8";
+        $mail->SMTPAuth = true;
+
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;
+
+        //Login Gmail
+        $mail->Username = "bazaronline.expcri@gmail.com";
+        $mail->Password = "nyrj lbvw rvgb oqzt";
+
+        //Remetente
+        $mail->setFrom("bazaronline.expcri@gmail.com", "Doe Amor");
+
+        //Destinatário
+        $mail->addAddress($email);
+
+        //Conteúdo do email
+        $mail->isHTML(true);
+        $mail->Subject = "Confirmação de conta";
+
+        $mail->Body = "
+        <h2>Confirme sua conta</h2>
+        <p>Clique no link abaixo para confirmar seu e-mail:</p>
+        <a href='http://localhost/bazar/index.html'>
+        Confirmar e-mail
+        </a>
+        ";
+
+        $mail->send();
+
+        echo "<h2>Email enviado com sucesso!</h2>";
+        echo "<p>Verifique sua caixa de entrada.</p>";
+
+    } catch (Exception $e) {
+
+        echo "Erro ao enviar email: {$mail->ErrorInfo}";
+
+    }
+
+}
+
 ?>
